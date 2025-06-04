8a61ad2179aed72a4a1bb8c9fa07e650e6a5e6e5
rizzsv <rizq.syafriano@gmail.com>
2025-06-04 10:29:52 +0700

    new update for fetching

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,16 @@ import express from "express";
 import userRoutes from "./interface/route/user.routes";
 import productRoutes from "./interface/route/product.routes";
 import transactionRoutes from "./interface/route/transaction.routes";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
+
+app.use(cors({
+    origin: "http://localhost:3000",
+    credentials: true
+}));
 
 app.use("/users", userRoutes);
 app.use("/products", productRoutes);
